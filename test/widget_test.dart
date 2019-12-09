3b51ee53e1d414423b5bc4dea6ef26c0cddbf3d9
Piotr Wicherski <pwicherski@gmail.com>
2019-12-09 09:59:36 +0100

    First widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,24 @@
 // This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Run with flutter test test/widget_test.dart
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter/material.dart';
 import 'package:first_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Main widget test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Find out when to get up if you go to bed right now'), findsOneWidget);
+    expect(find.text('If you head up'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('zZz'));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Refresh zZz'), findsOneWidget);
+    expect(find.text('If you head to bed right now, you should try to wake up at one of the following times\u{00B9}:'), findsOneWidget);
   });
 }
